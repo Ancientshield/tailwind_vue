5a8c93eee4583b66ffcddacf4a0d2a5f26ea4f4d
Ancientshield <ancientsword520@gmail.com>
2022-12-27 17:18:25 +0800

    Day3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <script setup></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <h1 class="text-7xl font-bold underline">Hello Tailwind</h1>
+  <br />
+  <div>
+    <h3 class="text-2xl font-blod text-center text-green-500">
+      什麼是兔兔教？?🐰
+    </h3>
+    <p>
+      一種兔兔的信仰教派，本派信奉真神兔兔，且每時每刻都將兔兔放在心中，期待兔兔降臨為我們出生時所帶的原紅蘿蔔通通吃乾淨，以讓我們可以進兔兔天國。
+    </p>
+    <div>
+      <ul class="list-decimal list-inside">
+        <li>每天都吃紅蘿蔔</li>
+        <li>不吃紅蘿蔔就吃菜</li>
+      </ul>
+    </div>
+    <div>
+      <button class="underline text-blue-500">
+        <b><i>信兔兔，無 Bug</i></b>
+      </button>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
